7a3a3c7f5ed1723e8816a00b02bdcc7173ffe56f
vitalygrishin27 <vitalygrishin27@gmail.com>
2026-05-26 17:12:42 +0200

    1

--- src/helper/apiClient.js
+++ src/helper/apiClient.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const apiClient = () => {
    //const MAIN_ENDPOINT = "https://microservice-voting.herokuapp.com/client/";
-   const MAIN_ENDPOINT = "http://192.168.0.213:8080/client/";
+   // const MAIN_ENDPOINT = "http://192.168.0.213:8080/client/";
+    const MAIN_ENDPOINT = "/api";
 
     return axios.create({
         baseURL: MAIN_ENDPOINT
